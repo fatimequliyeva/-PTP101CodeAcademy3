2f3ab0a85a440baa733fc8e3f96a83c33aaee14a
Your Name <fatimashg@code.edu.az>
2025-11-26 02:01:50 +0400

    my-first-javascript

---
+++ 14.javaScript/assest/script.js
@@ -0,0 +1,129 @@
+
+// const person = {
+//     name: "Fatime",
+//     age: 26,
+//     isstudent: true,
+//     hobbies: ['biker', 'dance'],
+//     address: "Iqor Agayev 3, home 4"
+// };
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.isstudent);
+// console.log(person.hobbies);
+// console.log(person.address);
+// console.log(person)
+
+
+
+
+// let num1 = 15;
+// let num2 = 27;
+
+// let sum = num1 + num2;
+// let difference = num1 - num2;
+// let product = num1 * num2;
+// let quotient = num1 / num2;
+// let remainder = num1 % num2;
+
+// console.log("Sum:", sum); //toplama
+// console.log("Difference:", difference); //cixma
+// console.log("Product:", product); //vurma
+// console.log("Quotient:", quotient); //bolme
+// console.log("Remainder:", remainder); //faiz-qaliq
+
+
+// 3
+// const person = {
+//     name: "Fatime",
+//     age: 26,
+//     isStudent: true,
+//     address: "Iqor Agayev 3, home 4",
+//     numbers: [18, 26,]
+// };
+
+
+// console.log(typeof person.name); // string
+// console.log(typeof person.age); // number
+// console.log(typeof person.isStudent); // boolean
+// console.log(typeof person.address); 
+// console.log(typeof person.numbers); // object
+// console.log(Array.isArray(person.numbers)); //array olduqunu yoxlamaq ucun yazdim
+
+// let celsius = 28;
+// let fahrenheit = celsius * 1.8 + 32;
+
+
+// console.log("Fahrenheit:", fahrenheit);
+
+
+// let number =18
+// console.log(number % 2 === 0 ? "even" : "odd");
+
+
+
+// let monthSalary =1250;
+// let yearSalary = monthSalary * 12;
+
+// console.log("Yearly Salary:", yearSalary);
+
+
+
+
+
+// let firstName = "Fatima";
+// let lastName = "Guliyeva";
+
+// let fullName = `My full name ${firstName} ${lastName}`;
+// console.log(fullName);
+
+
+
+
+// let r = 8;
+// let PI = 24;  //bunu tam anlamadm ne yalan deyim
+// let S = PI * r * r;
+// console.log(S);
+
+
+
+// let sentence = "JavaScript is awesome";
+// let length = sentence.length;   //metnin uzunluqudu bunuda tam anlamis sayilmam
+// console.log(length );
+
+
+
+
+
+// let price = 120;
+// let discount = 20;
+// let discounted = price - (price * discount / 50);
+// console.log(discounted);
+
+// function calculateDiscountedPrice(price, discount) {
+//     return price - (price * discount / 100);   //bunun funksiyanllasmis halidi ve bu o demekdirki bu kod tekrar isdifade oluna bilecek veziyyetdedi
+// }
+// let discountedPrice = calculateDiscountedPrice(120, 20);
+// console.log(discountedPrice); // 96
+
+
+
+
+
+let a = 12, b = 45, c = 32;
+let maxnum = Math.max(a, b, c); //en boyuk eded
+console.log(maxnum); // 45
+
+
+let d= 12, f= 45, n= 32;
+let minNum = Math.min(d,f,n); //en kicik eded
+console.log(minNum); // 12
+
+
+// let name3 = "Fatime";
+// let age3 = 26;
+// let city = "Baku";
+// let hobbies ="Biker";
+// let favColor ="Grey";
+// let bestFriend ="Nargiz";
+// console.log(`${name3} is ${age3} years old and lives in ${city}.${name3} favorite hobbies motosicle ${hobbies}.${name3} favortite color is ${favColor}.${name3} best friend is ${bestFriend}.`);
