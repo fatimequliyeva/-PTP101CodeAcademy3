fe7c6f6b0f82fa94f8ee96deef047382ca15d58d
Your Name <fatimashg@code.edu.az>
2025-12-11 00:12:51 +0400

    arrey-Methods

--- 20.js-array-methods/assest/js/script.js
+++ 20.js-array-methods/assest/js/script.js
@@ -2,12 +2,12 @@ const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
 shoppingCart.unshift("Meat")
 shoppingCart.push("Sugar")
 
-let i = shoppingCart.indexOf("Honey");
+let i = shoppingCart.indexOf("Honey");   //1
 if (i !== -1) {
     shoppingCart.splice(i, 1);
 }
 
-// Tea -> Green Tea etmək
+
 let tea = shoppingCart.indexOf("Tea");
 if (tea !== -1) {
     shoppingCart[tea] = "Green Tea";
@@ -34,7 +34,7 @@ countries.reverse()
 
 const indexDenmark = countries.indexOf("Denmark");
 const indexCanada =countries.indexOf("Canada");
-const indexAzerbaijan =countries.indexOf("Azerbaijan");
+const indexAzerbaijan =countries.indexOf("Azerbaijan");   //2
 
 if(indexCanada !== -1) countries.splice(indexCanada ,1)
 if(indexDenmark !== -1) countries.splice(indexDenmark ,1) 
@@ -46,7 +46,7 @@ console.log(countries);
 
 
 
-const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']   //3
 const backEnd = ['Node','Express', 'MongoDB']
 const fullStack = frontEnd.concat(backEnd)
 
@@ -57,33 +57,19 @@ console.log(fullStack);
 
 
 const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
-
-
 const firstThree = itCompanies.slice(0, 3);
-
-
 const lastThree = itCompanies.slice(-3);
-
-
-const middleStart = Math.floor(itCompanies.length / 2) - 1;
-const middleEnd = middleStart + 3;
+const middleStart = Math.floor(itCompanies.length / 2) - 1;  //4
+const middleEnd = middleStart + 2;
 const middleCompanies = itCompanies.slice(middleStart, middleEnd);
-
-
 const sortedCompanies = itCompanies.slice().sort();
-
 const upperCompanies = itCompanies.map(company => company.toUpperCase());
-
-
 const checkGoogle = itCompanies.includes("Google") ? "Google movcuddur" : "Company is not found";
-
 const companiesWithO = [];
 for (let company of itCompanies) {
     const countO = company.toLowerCase().split('').filter(ch => ch === 'o').length;
     if (countO >= 2) companiesWithO.push(company);
 }
-
-
 console.log("İlk 3 sirket:", firstThree);
 console.log("Son 3 sirket:", lastThree);
 console.log("Orta sirketler:", middleCompanies);
@@ -93,7 +79,70 @@ console.log( checkGoogle);
 console.log("2 ve ya daha cox 'o' herfi olan sirketler:", companiesWithO);
 
 
+
 function number(arr1, arr2, char) {
-    return arr1.concat(arr2).join(char);
+    return arr1.concat(arr2).join(char);   //5
 }
 console.log(number([1, 2], [3, 4], '*'))
+
+
+
+
+console.log("-----------------------------------------------");
+
+
+
+const arr = [
+  { name: 'test',     key: 1 },
+  { name: 'task',     key: 2 },
+  { name: 'tanqo',    key: 3 },
+  { name: 'like',     key: 4 },
+  { name: 'task',     key: 5 },
+  { name: 'trust',    key: 6 },
+  { name: 'test',     key: 7 },
+  { name: 'last',     key: 8 },
+  { name: 'tanqo',    key: 9 },
+  { name: 'elephant', key: 10 },
+  { name: 'love',     key: 11 },
+  { name: 'small',    key: 12 },
+  { name: 'little',   key: 13 },
+]; 
+
+const newArr = arr.filter(item => item.name.toLowerCase().includes("t"));  //ilk hrfi t olan 1
+console.log(newArr);
+
+const newArr2 = arr.filter(item => item.name.toLowerCase().startsWith("t"));  //icinde t olan ilk son 2
+console.log(newArr2);
+
+const longest = arr.reduce((max, item) => {
+  return item.name.length > max.name.length ? item : max; //en uzun elemet3
+});
+
+console.log(longest);
+
+const long = arr.reduce((max, item) => {
+  return item.name.length > max.name.length ? item : max;  //en uzun acar(key) 4
+});
+
+console.log(long.key);
+
+
+const newArr3 = arr.filter(item => item.name.length === 4);  //filter ile 4 elemet tutani yiqdim arreye 5
+
+console.log(newArr3);
+
+const result = arr.filter(item => 
+  item.key > 10 && item.name.toLowerCase().startsWith("l")  //hem l ile baslasin hemde keyi 10 den boyuk  olsun 6
+);
+
+console.log(result);
+
+
+const sumKeys = arr
+  .filter(item => {
+    const name = item.name.toLowerCase();
+    return name.startsWith("t") && name.endsWith("t"); //t ile baslayib t ile bitenin keylerin cemi 7
+  })
+  .reduce((sum, item) => sum + item.key, 0);
+
+console.log(sumKeys);
