f5bf0267170c9bd6ae7d95613602abb1db22e4b5
Your Name <fatimashg@code.edu.az>
2025-12-21 02:52:48 +0400

    my-dom-2

---
+++ 22.js-task-dom/assest/script/script.js
@@ -0,0 +1,12 @@
+// const openBtn = document.getElementById("openBtn");
+// const sidebar = document.getElementById("sidebar");
+// const closeBtn = document.getElementById("closeBtn");
+
+// openBtn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   sidebar.classList.remove("active");
+// });
+
