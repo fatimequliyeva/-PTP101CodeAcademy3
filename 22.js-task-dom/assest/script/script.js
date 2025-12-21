9066f78e2cf0982baf79a0b1f7ca8adf39c8a99e
Your Name <fatimashg@code.edu.az>
2025-12-22 01:00:28 +0400

    my-dom-sidebar

--- 22.js-task-dom/assest/script/script.js
+++ 22.js-task-dom/assest/script/script.js
@@ -1,12 +1,12 @@
-// const openBtn = document.getElementById("openBtn");
-// const sidebar = document.getElementById("sidebar");
-// const closeBtn = document.getElementById("closeBtn");
+const openBtn = document.getElementById("openBtn");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("closeBtn");
 
-// openBtn.addEventListener("click", () => {
-//   sidebar.classList.toggle("active");
-// });
+openBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
 
-// closeBtn.addEventListener("click", () => {
-//   sidebar.classList.remove("active");
-// });
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
 
