c4a437f9e97fe9f133f145082c84a0312347e346
Your Name <fatimashg@code.edu.az>
2026-01-06 20:57:20 +0400

    My-Blog-task-added

--- 25-block-app/js/main.js
+++ 25-block-app/js/main.js
@@ -15,13 +15,39 @@ function renderBlogs(blogs) {
     const card = document.createElement("div");
     card.className = "card";
 
+    const fullText = blog.body;
+    const shortText = fullText.length > 100 ? fullText.substring(0, 100) : fullText;
+
     card.innerHTML = `
       <h3>${blog.title}</h3>
-      <p>${blog.body.substring(0, 100)}...</p>
+      <p class="blog-body">${shortText} ${
+        fullText.length > 100 ? '<span class="read-more">Read More</span>' : ""
+      }</p>
       <small>Author: ${blog.author}</small><br/>
       <button class="delete-btn">Delete</button>
       <a href="edit.html?id=${blog.id}">Edit</a>
     `;
+
+    const bodyElem = card.querySelector(".blog-body");
+    const moreElem = card.querySelector(".read-more");
+
+    if (moreElem) {
+      moreElem.style.color = "#0d00ffff";
+      moreElem.style.cursor = "pointer";
+      moreElem.style.fontWeight = "bold";
+
+      moreElem.addEventListener("click", () => {
+        if (moreElem.innerText === "Read More") {
+          bodyElem.innerText = fullText + " ";
+          moreElem.innerText = "Read Less";
+          bodyElem.appendChild(moreElem);
+        } else {
+          bodyElem.innerText = shortText + (fullText.length > 100 ? " " : "");
+          moreElem.innerText = "Read More";
+          bodyElem.appendChild(moreElem);
+        }
+      });
+    }
 
     card.querySelector(".delete-btn").addEventListener("click", () => {
       deleteBlog(blog.id);
