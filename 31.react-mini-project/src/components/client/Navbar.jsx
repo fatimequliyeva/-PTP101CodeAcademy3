7ea50033a37b53f4b7ff8de42e72ca0d5725d38d
Your Name <fatimashg@code.edu.az>
2026-02-16 11:17:20 +0400

    my-ham-menyu

--- 31.react-mini-project/src/components/client/Navbar.jsx
+++ 31.react-mini-project/src/components/client/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { FaShoppingCart, FaHeart } from 'react-icons/fa';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { FaShoppingCart, FaHeart, FaBars } from 'react-icons/fa';
 import styles from './Navbar.module.css';
 import { useShop } from '../../context/ShopContext';
 
 const Navbar = () => {
   const { basket, wishlist } = useShop();
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const onScroll = () => {
@@ -20,13 +22,52 @@ const Navbar = () => {
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
+
+  useEffect(() => {
+    const onResize = () => {
+      if (window.innerWidth > 768) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location.pathname]);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
   return (
     <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
       <div className="container">
         <div className={styles['navbar-content']}>
-          <Link to="/" className={styles['navbar-logo']}>VEGEFOODS</Link>
-          
-          <ul className={styles['navbar-links']}>
+          <div className={styles['navbar-brand']}>
+            <Link to="/" className={styles['navbar-logo']}>VEGEFOODS</Link>
+            <button
+              type="button"
+              className={styles['menu-toggle']}
+              aria-label="Toggle menu"
+              aria-controls="primary-navigation"
+              aria-expanded={menuOpen}
+              onClick={toggleMenu}
+            >
+              <FaBars />
+            </button>
+          </div>
+
+          <ul
+            id="primary-navigation"
+            className={`${styles['navbar-links']} ${menuOpen ? styles['navbar-links-open'] : ''}`}
+            onClick={closeMenu}
+          >
             <li><NavLink to="/" end>HOME</NavLink></li>
             <li className={styles.dropdown}>
               <NavLink to="/shop">SHOP</NavLink>
